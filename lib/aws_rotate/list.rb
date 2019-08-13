538243fb116a5ec5b5c53e276f9baac335eb4b0e
Tung Nguyen <tongueroo@gmail.com>
2019-08-13 19:58:11 +0000

    cleanup

--- lib/aws_rotate/list.rb
+++ lib/aws_rotate/list.rb
@@ -7,14 +7,18 @@ module AwsRotate
     end
 
     def run
+      puts "AWS Profiles:"
+      puts profiles
+    end
+
+    def profiles
       lines = IO.readlines(@credentials_path)
       profiles = []
       lines.each do |line|
         md = line.match(/\[(.*)\]/)
         profiles << md[1] if md
       end
-      puts "AWS Profiles:"
-      puts profiles
+      profiles
     end
   end
 end
